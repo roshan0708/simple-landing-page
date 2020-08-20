c3aa5797753e56291903ea75e281e4db73d45c5a
Roshan Kanwar <roshan.18je0696@am.iitism.ac.in>
2020-08-20 21:57:59 +0530

    sample images and logo added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Product1 from "./assests/img/main-logo.svg";
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       Hello World
+      <img src={Product1} alt=""/>
     </div>
   );
 }
